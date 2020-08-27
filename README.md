3fe4eeb0696a13357c33f6b1cde09ee79b2c56d7
claire p <cpuckett9@gmail.com>
2020-08-27 14:04:24 -0500

    UX enhancements

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## taskmaster-pro
 
 ## edit function doesn't work in mobile view(bug? on purpose or no?)
+
+## 5.5.4 - console.log doesn't register date when set to 5000 interval. is the audit interval working?
--- assets/js/script.js
+++ assets/js/script.js
@@ -170,16 +170,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    // console.log("activate", this);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
   deactivate: function (event) {
-    // console.log("deactivate", this);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function (event) {
-    // console.log("over", event.target);
+    $(event.target).addClass("dropover-active")
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function (event) {
-    // console.log("out", event.target);
+   $(event.target).removeClass("dropover-active")
+   $(".bottom-trash").removeClass("bottom-trash-active")
   },
   update: function (event) {
     //array to store the task data in
@@ -229,7 +233,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -277,6 +281,7 @@ var auditTask = function (taskEl) {
 
   // this should print out an object for the value of the date variable, but at 5:00pm of that date
   console.log(time);
+  console.log(taskEl)
   
 }
 
@@ -307,6 +312,15 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
+
+//repeat auditTask function at a given time interval. goes thru each element with class list-group-item and audits each one
+setInterval(function(){
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el)
+    
+  })
+}, 180000)
+//or (1000*60)*30)
 
 
 
